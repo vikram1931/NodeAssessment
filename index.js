07f806f77bec750bcab49b3483fab77d38bc4cd0
vikram1931 <34970538+vikram1931@users.noreply.github.com>
2022-05-02 13:23:47 -0400

    git commit -m ' apiposts updated'

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-let topArray = [];
-
 app.listen(3065, () => {
   console.log("server running on  port 3065");
 });
@@ -13,47 +11,35 @@ app.get("/api/ping", (req, res) => {
   });
 });
 
-app.get("/api/posts", (request, res) => {
+app.get("/api/posts", (request, result) => {
   // const tag = req.query.tag;
 
   const array = [
     "tech",
     "health",
-    "histroy",
-    "startups",
-    "science",
-    "design",
-    "culture",
-    "politics",
+    //  // "histroy",
+    //  // "startups",
+    //   //"science",
+    //   "design",
+    //   "culture",
+    //   "politics",
   ];
 
-  let combinedArray = [];
+  const combinedArray = [];
   for (let i = 0; i < array.length; i++) {
     const request = require("request");
-    //  const raw=[]
+
     request(
       `https://api.hatchways.io/assessment/blog/posts?tag=${array[i]}`,
       function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          // console.log(body); // Print the google web page.
-          // console.log(JSON.parse(body));
-
           const bodycopy = JSON.parse(body);
-          // console.log(bodycopy);
           combinedArray.push(...bodycopy.posts);
-          topArray = [...combinedArray];
-          console.log(topArray);
-          //.............................................................
+
           //console.log(combinedArray);
-          //  combinedArray = [...combinedArray, { ...bodycopy }];
-          // console.log(Array.isArray(bodycopy.posts));
+          result.send(combinedArray);
         }
       }
     );
   }
 });
-
-// app.get("/api/posts", (req, res) => {
-//   let tag = req.query.tag;
-//   console.log("tag");
-// });
